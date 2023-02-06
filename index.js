235466a7b161f8729f91eed57c256483ecf6b1fe
Andrei Trunkin <inboxavt@gmail.com>
2023-02-06 21:38:52 +0100

    add JS-code for the game

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Tic-Tac-Toe');
+// const goToPlay = () => {
+  const getTable = document.querySelector('.game');
+  let cellText = 'X';
+  getTable.addEventListener('click', (event) => {
+    const { target } = event;
+    if (target.textContent === '') {
+      target.textContent = cellText;
+    }
+    if (target.textContent === 'X' || target.textContent === 'O') {
+      cellText = (cellText === 'X') ? 'O' : 'X';
+    }
+  });
+// };
