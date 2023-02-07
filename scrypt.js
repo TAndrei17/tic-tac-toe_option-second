b6e5436185c4de07acb9e070b2b5853aab7a8a7e
Andrei Trunkin <inboxavt@gmail.com>
2023-02-07 12:46:04 +0100

    update files by prettier

--- scrypt.js
+++ scrypt.js
@@ -1,11 +1,11 @@
-  const getTable = document.querySelector('.game');
-  let cellText = 'X';
-  getTable.addEventListener('click', (event) => {
-    const { target } = event;
-    if (target.textContent === '') {
-      target.textContent = cellText;
-    }
-    if (target.textContent === 'X' || target.textContent === 'O') {
-      cellText = (cellText === 'X') ? 'O' : 'X';
-    }
-  });
+const getTable = document.querySelector(".game");
+let cellText = "X";
+getTable.addEventListener("click", (event) => {
+  const { target } = event;
+  if (target.textContent === "") {
+    target.textContent = cellText;
+  }
+  if (target.textContent === "X" || target.textContent === "O") {
+    cellText = cellText === "X" ? "O" : "X";
+  }
+});
