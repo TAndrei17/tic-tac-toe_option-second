6186b4fea1e7ad78f9c2818eb296a9f662664f68
Andrei Trunkin <inboxavt@gmail.com>
2023-02-07 22:26:42 +0100

    created game-field by JS

--- scrypt.js
+++ scrypt.js
@@ -1,11 +1,32 @@
-const getTable = document.querySelector('.game');
-let cellText = 'X';
-getTable.addEventListener('click', (event) => {
-  const { target } = event;
-  if (target.textContent === '') {
-    target.textContent = cellText;
+const createField = () => {
+  const tableBox = document.createElement('table');
+  tableBox.classList.add('game');
+  for (let i = 0; i < 3; i += 1) {
+    const str = tableBox.insertRow();
+    str.classList.add('line');
+    for (let j = 0; j < 3; j += 1) {
+      const cell = str.insertCell();
+      cell.classList.add('cell');
+      cell.innerHTML = '<span class="xo"></span>';
+    }
   }
-  if (target.textContent === 'X' || target.textContent === 'O') {
-    cellText = cellText === 'X' ? 'O' : 'X';
-  }
-});
+  return tableBox;
+};
+
+const goToPlay = () => {
+  const tableAll = createField();
+  const divRoot = document.querySelector('.root');
+  divRoot.append(tableAll);
+  let cellText = 'X';
+  tableAll.addEventListener('click', (event) => {
+    const { target } = event;
+    if (target.textContent === '') {
+      target.textContent = cellText;
+    }
+    if (target.textContent === 'X' || target.textContent === 'O') {
+      cellText = cellText === 'X' ? 'O' : 'X';
+    }
+  });
+};
+
+goToPlay();
