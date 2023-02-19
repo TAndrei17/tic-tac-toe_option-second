e6fd8291b97cb57f90cf0d2c569945587b0e7918
Andrei Trunkin <inboxavt@gmail.com>
2023-02-19 12:24:04 +0100

    used more abstractions and corrected mistakes

--- scrypt.js
+++ scrypt.js
@@ -1,72 +1,76 @@
 import createField from './field_cell.js';
-import createFinalDiv from './final_div.js';
+import { createFinalDiv, createButtonReload } from './final_div.js';
 import {
   createCell,
   // collectCells,
   checkRows,
   checkColumns,
   checkDiagonal,
+  getRow,
+  getColumn,
 } from './my_abstractions.js';
 
 const startPlay = () => {
   const field = createField();
-  const divRoot = document.querySelector('.root');
+  const divRoot = document.querySelector('.container');
   divRoot.append(field);
-  const turn = document.querySelector('.step');
   let cellText = 'X';
-  document.body.prepend(turn);
   const allXs = [];
   const allOs = [];
+  const turn = document.querySelector('.step');
+  document.body.prepend(turn);
   turn.textContent = `'${cellText}' starts`;
   field.addEventListener('click', (event) => {
     const { target } = event;
     const eventRow = event.target.row;
     const eventColumn = event.target.column;
     const eventCell = createCell(eventRow, eventColumn);
-    // 'X' starts
     if (target.textContent === '') {
       target.textContent = cellText;
     }
-    // if cell is occupied nothing happen but turn goes to another player
     if (target.textContent === 'X' || target.textContent === 'O') {
       cellText = cellText === 'X' ? 'O' : 'X';
     }
-    turn.textContent = `'${cellText}' now`;
-    if ((target.textContent === 'X') && (!allXs.includes(eventCell))) {
-      // collect every step of 'X'
-      allXs.push(eventCell);
+    turn.textContent = `'${cellText}' go now`;
+    if (target.textContent === 'X') {
+      const a = getRow(eventCell);
+      const b = getColumn(eventCell);
+      const checkAllXs = allXs.filter((cell) => getRow(cell) === a && getColumn(cell) === b);
+      if (checkAllXs.length === 0) {
+        allXs.push(eventCell);
+      }
     }
-    if ((target.textContent === 'O') && (!allOs.includes(eventCell))) {
-      // collect every step of 'O'
-      allOs.push(eventCell);
+    if (target.textContent === 'O') {
+      const a = getRow(eventCell);
+      const b = getColumn(eventCell);
+      const checkAllOs = allOs.filter((cell) => getRow(cell) === a && getColumn(cell) === b);
+      if (checkAllOs.length === 0) {
+        allOs.push(eventCell);
+      }
     }
-    // check X's collection on every step
     const isThreeXInRow = checkRows(allXs);
     const isThreeXInColumn = checkColumns(allXs);
     const isThreeXInDiagonal = checkDiagonal(allXs);
     if ((isThreeXInRow) || (isThreeXInColumn) || (isThreeXInDiagonal)) {
       turn.remove();
-      // if winner appears is created <div> and the container receive class '.winner'
       const finalDiv = createFinalDiv('CONGRATS! \'X\' IS WINNER!');
       divRoot.classList.add('winner');
       document.body.append(finalDiv);
       field.replaceWith(finalDiv);
-      const butt = document.querySelector('div > button');
-      butt.addEventListener('click', () => document.location.reload());
+      const button = createButtonReload();
+      finalDiv.append(button);
     }
-    // check O's collection on every step
     const isThreeOInRow = checkRows(allOs);
     const isThreeOInColumn = checkColumns(allOs);
     const isThreeOInDiagonal = checkDiagonal(allOs);
     if ((isThreeOInRow) || (isThreeOInColumn) || (isThreeOInDiagonal)) {
       turn.remove();
-      // if winner appears is created <div> and the container receive class '.winner'
       const finalDiv = createFinalDiv('CONGRATS! \'O\' IS WINNER!');
       divRoot.classList.add('winner');
       document.body.append(finalDiv);
       field.replaceWith(finalDiv);
-      const butt = document.querySelector('div > button');
-      butt.addEventListener('click', () => document.location.reload());
+      const button = createButtonReload();
+      finalDiv.append(button);
     }
     const getWinner = document.querySelector('.winner');
     if (([...allXs, ...allOs].length === 9) && (!getWinner)) {
@@ -75,8 +79,8 @@ const startPlay = () => {
       divRoot.classList.add('winner');
       document.body.append(finalDiv);
       field.replaceWith(finalDiv);
-      const butt = document.querySelector('div > button');
-      butt.addEventListener('click', () => document.location.reload());
+      const button = createButtonReload();
+      finalDiv.append(button);
     }
   });
 };
