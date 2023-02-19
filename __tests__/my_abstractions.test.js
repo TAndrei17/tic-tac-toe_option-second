e3c0024f2391faa34fd3b593cc4e635358ab4aa6
Andrei Trunkin <inboxavt@gmail.com>
2023-02-19 16:02:06 +0100

    updated tests and corrected mistakes

--- __tests__/my_abstractions.test.js
+++ __tests__/my_abstractions.test.js
@@ -15,31 +15,45 @@ test('cteateCell', () => {
 
 test('getRow', () => {
   expect(getRow(createCell(1, 1))).toEqual(1);
+  expect(getRow(createCell(0, 0))).toEqual(0);
 });
 
 test('getColumn', () => {
   expect(getColumn(createCell(1, 3))).toEqual(3);
+  expect(getColumn(createCell(0, 0))).toEqual(0);
 });
 
 test('checkRows', () => {
   expect(checkRows([(createCell(1, 1)), createCell(1, 2), createCell(1, 3)])).toEqual(true);
   expect(checkRows([(createCell(1, 1)), createCell(2, 1), createCell(1, 3)])).toEqual(false);
   expect(checkRows([(createCell(1, 1)), createCell(1, 3)])).toEqual(false);
+  // decided below problem in 'scrypt.js' but it's possible to decide in 'my_abstractions.js' too;
+  // abstraction functions could to filter repeted elements of array,
+  // but i use [...allXs, ...allOs].length for finishing in case of draw,
+  // that reason filter repeted cells in the moment of 'click.event'
+  expect(checkRows([(createCell(1, 1)), createCell(1, 1), createCell(1, 1)])).toEqual(true);
 });
 
 test('checkColumns', () => {
   expect(checkColumns([(createCell(1, 1)), createCell(2, 1), createCell(3, 1)])).toEqual(true);
   expect(checkColumns([(createCell(1, 1)), createCell(2, 1), createCell(3, 2)])).toEqual(false);
   expect(checkColumns([(createCell(1, 1)), createCell(2, 1)])).toEqual(false);
+  // decided below problem in 'scrypt.js' but it's possible to decide in 'my_abstractions.js' too;
+  // abstraction functions could to filter repeted elements of array,
+  // but i use [...allXs, ...allOs].length for finishing in case of draw,
+  // that reason filter repeted cells in the moment of 'click.event'
+  expect(checkColumns([(createCell(1, 1)), createCell(1, 1), createCell(1, 1)])).toEqual(true);
 });
 
 test('checkDiagonal', () => {
   expect(checkDiagonal([(createCell(1, 1)), createCell(2, 2), createCell(3, 3)])).toEqual(true);
-  expect(checkDiagonal([(createCell(1, 3)), createCell(2, 2), createCell(1, 3)])).toEqual(true);
+  expect(checkDiagonal([(createCell(1, 3)), createCell(2, 2), createCell(3, 1)])).toEqual(true);
   expect(checkColumns([(createCell(1, 1)), createCell(2, 2), createCell(1, 3)])).toEqual(false);
-  expect(checkColumns([(createCell(1, 3)), createCell(2, 2), createCell(3, 1)])).toEqual(false);
+  expect(checkColumns([(createCell(1, 3)), createCell(2, 2), createCell(3, 3)])).toEqual(false);
   expect(checkColumns([(createCell(1, 3)), createCell(2, 2)])).toEqual(false);
-  // decided this problem in 'scrypt.js' but it's possible to decide in 'my_abstractions.js';
-  // (a idea) the function could to delete repeted elements of array;
-  expect(checkColumns([(createCell(1, 3)), createCell(1, 3), createCell(1, 3)])).toEqual(false);
+  // decided below problem in 'scrypt.js' but it's possible to decide in 'my_abstractions.js' too;
+  // abstraction functions could to filter repeted elements of array,
+  // but i use [...allXs, ...allOs].length for finishing in case of draw,
+  // that reason filter repeted cells in the moment of 'click.event'
+  expect(checkColumns([(createCell(1, 3)), createCell(1, 3), createCell(1, 3)])).toEqual(true);
 });
--- scrypt.js
+++ scrypt.js
@@ -1,5 +1,5 @@
-import createField from "./field_cell.js";
-import { createFinalDiv, createButtonReload } from "./final_div.js";
+import createField from './field_cell.js';
+import { createFinalDiv, createButtonReload } from './final_div.js';
 import {
   createCell,
   // collectCells,
@@ -8,46 +8,46 @@ import {
   checkDiagonal,
   getRow,
   getColumn,
-} from "./my_abstractions.js";
+} from './my_abstractions.js';
 
 const startPlay = () => {
   const field = createField();
-  const divRoot = document.querySelector(".container");
+  const divRoot = document.querySelector('.container');
   divRoot.append(field);
-  let cellText = "X";
+  let cellText = 'X';
   const allXs = [];
   const allOs = [];
-  const turn = document.querySelector(".step");
+  const turn = document.querySelector('.step');
   document.body.prepend(turn);
   turn.textContent = `'${cellText}' starts`;
-  field.addEventListener("click", (event) => {
+  field.addEventListener('click', (event) => {
     const { target } = event;
     const eventRow = event.target.row;
     const eventColumn = event.target.column;
     const eventCell = createCell(eventRow, eventColumn);
     console.log(eventCell);
-    if (target.textContent === "") {
+    if (target.textContent === '') {
       target.textContent = cellText;
     }
-    if (target.textContent === "X" || target.textContent === "O") {
-      cellText = cellText === "X" ? "O" : "X";
+    if (target.textContent === 'X' || target.textContent === 'O') {
+      cellText = cellText === 'X' ? 'O' : 'X';
     }
     turn.textContent = `'${cellText}' go now`;
-    if (target.textContent === "X") {
+    if (target.textContent === 'X') {
       const a = getRow(eventCell);
       const b = getColumn(eventCell);
       const checkAllXs = allXs.filter(
-        (cell) => getRow(cell) === a && getColumn(cell) === b
+        (cell) => getRow(cell) === a && getColumn(cell) === b,
       );
       if (checkAllXs.length === 0) {
         allXs.push(eventCell);
       }
     }
-    if (target.textContent === "O") {
+    if (target.textContent === 'O') {
       const a = getRow(eventCell);
       const b = getColumn(eventCell);
       const checkAllOs = allOs.filter(
-        (cell) => getRow(cell) === a && getColumn(cell) === b
+        (cell) => getRow(cell) === a && getColumn(cell) === b,
       );
       if (checkAllOs.length === 0) {
         allOs.push(eventCell);
@@ -59,7 +59,7 @@ const startPlay = () => {
     if (isThreeXInRow || isThreeXInColumn || isThreeXInDiagonal) {
       turn.remove();
       const finalDiv = createFinalDiv("CONGRATS! 'X' IS WINNER!");
-      divRoot.classList.add("winner");
+      divRoot.classList.add('winner');
       document.body.append(finalDiv);
       field.replaceWith(finalDiv);
       const button = createButtonReload();
@@ -71,17 +71,17 @@ const startPlay = () => {
     if (isThreeOInRow || isThreeOInColumn || isThreeOInDiagonal) {
       turn.remove();
       const finalDiv = createFinalDiv("CONGRATS! 'O' IS WINNER!");
-      divRoot.classList.add("winner");
+      divRoot.classList.add('winner');
       document.body.append(finalDiv);
       field.replaceWith(finalDiv);
       const button = createButtonReload();
       finalDiv.append(button);
     }
-    const getWinner = document.querySelector(".winner");
+    const getWinner = document.querySelector('.winner');
     if ([...allXs, ...allOs].length === 9 && !getWinner) {
       turn.remove();
-      const finalDiv = createFinalDiv("DRAW!");
-      divRoot.classList.add("winner");
+      const finalDiv = createFinalDiv('DRAW!');
+      divRoot.classList.add('winner');
       document.body.append(finalDiv);
       field.replaceWith(finalDiv);
       const button = createButtonReload();
